01e77cea26a633f59985b495ba60a1e9580c69a0
jimareed <jim.a.reed@gmail.com>
2020-11-24 17:27:41 -0500

    copied basic login code from react-auth0-login
    copied basic login code from react-auth0-login

--- src/Profile.js
+++ src/Profile.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "./react-auth0-spa";
 
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const { user, isAuthenticated, loading } = useAuth0();
 
-  return (
-    isAuthenticated && (
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+
+  if (!isAuthenticated || !user) {
+    return (
       <div>
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <p>Select <b>Login</b> to continue.</p>
       </div>
     )
-  );
+  }
+
+  return (
+      <div>
+        <p><b>Welcome {user.name}!</b> You are logged in. </p>
+      </div>
+     )
 };
 
 export default Profile;
